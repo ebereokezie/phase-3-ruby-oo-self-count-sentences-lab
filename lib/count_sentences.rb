de9fd2b5cb3c4141e533a77072e7ec44190e49a5
Ebere Okezie <ebereokezie@gmail.com>
2022-08-17 16:08:37 -0500

    commit tree abca0d249e587c7706de471c18665dcb533e9cbf

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,20 @@ class String
 
   def sentence?
     
+    self.end_with?(".")
+  
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).reject{|string| string.empty?}.count
   end
+  binding.pry
 end
